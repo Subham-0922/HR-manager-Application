e6a2df49369c1fc7be562b2b1563d63bdfe864d0
Subham-0922 <ksubham789@gmail.com>
2023-03-31 12:17:03 +0530

    all done day 3

--- src/daoPackage/AdminDAOImpl.java
+++ src/daoPackage/AdminDAOImpl.java
@@ -5,7 +5,9 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -253,6 +255,126 @@ public class AdminDAOImpl implements AdminDAO {
 			}
 		}
 		
+	}
+
+	@Override
+	public List<String> getListOFPendingLeaveRequest() throws SomeThingWentWrong {
+		// TODO Auto-generated method stub
+		Connection c=null;
+		DateTimeFormatter dt=DateTimeFormatter.ofPattern("dd/MM/YYYY");
+		List<String> list=new ArrayList<>();
+		try {
+			c=DBUtils.getConnection();
+			PreparedStatement ps=c.prepareStatement("select Ename,e.dname,days_of_leave,type,reason,Date_of_leave from empleave l join employee e on e.eid=l.eid where status='pending' && e.isRemoved=false");
+			ResultSet rs=ps.executeQuery();
+			if(DBUtils.isResultEmpty(rs)) {
+				throw new SomeThingWentWrong("There is no pending leave request available");
+			}
+			while(rs.next()) {
+				String info=rs.getString(1)+" from Department "+rs.getString(2)+" requested for "+rs.getInt(3)+" days of leave under "+ (rs.getInt(4)==1 ? "complementry" : rs.getInt(4)==2 ? "sick" : "extra leave")+" due to "+rs.getString(5)+" on "+dt.format(rs.getDate(6).toLocalDate());
+				list.add(info);
+			}
+			if(list.size()==0) {
+				throw new SomeThingWentWrong("There is no pending leave request available");
+				
+			}
+			return list;
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	@Override
+	public List<Integer> getListOfPendingLID() throws SomeThingWentWrong {
+		// TODO Auto-generated method stub
+		Connection c=null;
+		List<Integer> list=new ArrayList<>();
+		try {
+			c=DBUtils.getConnection();
+			PreparedStatement ps=c.prepareStatement("select lid from empleave l join employee e on e.eid=l.eid where status='pending' && e.isRemoved=false");
+			ResultSet rs=ps.executeQuery();
+			if(DBUtils.isResultEmpty(rs)) {
+				throw new SomeThingWentWrong("There is no pending leave request available");
+			}
+			while(rs.next()) {
+				list.add(rs.getInt(1));
+			}
+			if(list.size()==0) {
+				throw new SomeThingWentWrong("There is no pending leave request available");
+				
+			}
+			return list;
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	@Override
+	public void acceptLeave(int lid, String remark) throws SomeThingWentWrong {
+		// TODO Auto-generated method stub
+		Connection c=null;
+		int eid=0;
+		int type=0;
+		try {
+			c=DBUtils.getConnection();
+			PreparedStatement ps=c.prepareStatement("select eid,type from empleave where lid=?");
+			ps.setInt(1, lid);
+			ResultSet rs=ps.executeQuery();
+			if(DBUtils.isResultEmpty(rs)) {
+				throw new SomeThingWentWrong("Something went wrong");
+			}
+			while(rs.next()) {
+				eid=rs.getInt(1);
+				type=rs.getInt(2);
+			}
+			String val="";
+			if(type==1) {
+				val="available_Com_Leave";
+			}else if(type==2) {
+				val="available_sick_leave";
+			
+			}else {
+				val="leave_taken";
+			}
+			PreparedStatement ps2=c.prepareStatement("update empleave set status='Accepted',remark=? where lid=?");
+			ps2.setString(1, remark);
+			ps2.setInt(2, lid);
+			ps2.executeUpdate();
+			if(type==1||type==2) {
+				PreparedStatement ps3=c.prepareStatement("update employee set "+val+"="+val+"-1 where eid=?");
+				ps3.setInt(1, eid);
+				ps3.executeUpdate();
+			}else {
+				PreparedStatement ps3=c.prepareStatement("update employee set "+val+"="+val+"+1 where eid=?");
+				ps3.setInt(1, eid);
+				ps3.executeUpdate();
+			}
+			
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			throw new SomeThingWentWrong("Something Went Wrong");
+		}
+	}
+
+	@Override
+	public void rejectLeave(int lid, String remark) throws SomeThingWentWrong {
+		// TODO Auto-generated method stub
+		Connection c=null;
+		try {
+			c=DBUtils.getConnection();
+			PreparedStatement ps2=c.prepareStatement("update empleave set status='Rejected',remark=? where lid=?");
+			ps2.setString(1, remark);
+			ps2.setInt(2, lid);
+			ps2.executeUpdate();
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
